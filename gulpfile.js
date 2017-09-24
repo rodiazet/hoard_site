cbf43b95191149392064655d3abb2715afa3adfa
kuba <getkuba@gmail.com>
2017-09-24 14:31:29 +0700

    added build for contr and stats in global gulpfile, prepare during before after

--- gulpfile.js
+++ gulpfile.js
@@ -4,8 +4,28 @@ var deploy = require('gulp-deploy-git');
  
 gulp.task( 'build_all', function () {
     gulp.src( 'Markup/frontpage/gulpfile.js' )
-        .pipe( chug({tasks:['build']}) )
-} );
+        .pipe( chug({tasks:['build']}) );
+    gulp.src( 'Markup/contribution/gulpfile.js' )
+        .pipe( chug({tasks:['build']}) );
+    gulp.src( 'Markup/statistics/gulpfile.js' )
+        .pipe( chug({tasks:['build']}) );
+});
+
+gulp.task( 'prepare_during', function () {
+    gulp.src( 'Markup/contribution/gulpfile.js' )
+        .pipe( chug({tasks:['prepare_during']}) );
+});
+
+gulp.task( 'prepare_after', function () {
+    gulp.src( 'Markup/contribution/gulpfile.js' )
+        .pipe( chug({tasks:['prepare_after']}) );
+});
+
+gulp.task( 'prepare_before', function () {
+    gulp.src( 'Markup/contribution/gulpfile.js' )
+        .pipe( chug({tasks:['prepare_before']}) );
+});
+
 
 gulp.task('deploy', function() {
     return gulp.src('Markup/frontpage/build/**/*',{ read: false })
