8310106b82f9339f2f7801d235afda508dad100e
kuba <getkuba@gmail.com>
2017-09-24 14:16:43 +0700

    minify contribution

--- Markup/contribution/gulpfile.js
+++ Markup/contribution/gulpfile.js
@@ -7,9 +7,19 @@ var del = require('del');
 var cleanCSS = require('gulp-clean-css');
 var runSequence = require('run-sequence');
 var git = require('gulp-git');
+var htmlmin = require('gulp-htmlmin');
+var rename = require('gulp-rename-assets');
 
 gulp.task('minifyjs', function() {
-    return gulp.src('build/js/all.js')
+    gulp.src('build/js/before.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('build/js/minjs/'));
+
+    gulp.src('build/js/during.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('build/js/minjs/'));
+
+    return gulp.src('build/js/after.js')
     .pipe(uglify())
     .pipe(gulp.dest('build/js/minjs/'))
 });
@@ -17,47 +27,77 @@ gulp.task('minifyjs', function() {
 gulp.task('concatcss', function() {
     return gulp.src(['css/bootstrap.min.css',
     'css/font-awesome.min.css',
-    'css/jquery.classycountdown.min.css',
-    'css/lity.min.css',
-    'css/style.min.css'])
+    'css/contribution.min.css'])
     .pipe(concat('all.css'))
     .pipe(gulp.dest('build/css/'));
 });
 
-gulp.task('concatjs', function() {
+gulp.task('concatjs_before', function() {
     return gulp.src(['js/jquery-2.2.3.min.js',
     'js/modernizr-3.3.1.min.js',
     'js/bootstrap.min.js',
     'js/jquery.classycountdown.min.js',
     'js/jquery.knob.js',
     'js/jquery.throttle.js',
-    'js/slick.min.js',
-    'js/lity.min.js',
     'js/main.js',
-    'js/header.js',
-    'js/module-teaserbox.js',
     'js/module-countdown.js',
-    'js/module-investor.js',
-    'js/module-team.js',
-    'js/module-slideshow.js',
-    'js/mailchimp.js'])
-    .pipe(concat('all.js'))
+    'js/base-contribution.js'])
+    .pipe(concat('before.js'))
+    .pipe(gulp.dest('build/js/'));
+});
+gulp.task('concatjs_during', function() {
+    return gulp.src(['js/jquery-2.2.3.min.js',
+    'js/modernizr-3.3.1.min.js',
+    'js/bootstrap.min.js',
+    'js/jquery.classycountdown.min.js',
+    'js/jquery.knob.js',
+    'js/jquery.throttle.js',
+    'js/main.js',
+    'js/module-countdown.js',
+    'js/base-contribution.js',
+    'js/during-contribution.js',
+    'js/during-balance.js'])
+    .pipe(concat('during.js'))
+    .pipe(gulp.dest('build/js/'));
+});
+gulp.task('concatjs_after', function() {
+    return gulp.src(['js/jquery-2.2.3.min.js',
+    'js/modernizr-3.3.1.min.js',
+    'js/bootstrap.min.js',
+    'js/jquery.classycountdown.min.js',
+    'js/main.js'])
+    .pipe(concat('after.js'))
     .pipe(gulp.dest('build/js/'));
 });
 
 gulp.task('minifycss', function() {
-     return gulp.src('css/*.css')
+     return gulp.src('build/css/all.css')
       .pipe(cleanCSS({compatibility: 'ie8'}))
-      .pipe(gulp.dest('build/css/'));
+      .pipe(gulp.dest('build/css/mincss/all.css'));
   });
 
 gulp.task('replacehtml', function() {
-    return gulp.src('index.html')
+    gulp.src('during.html')
+    .pipe(htmlreplace({
+        'css':'css/all.css',
+        'js': 'js/minjs/during.js'
+    }))
+    .pipe(gulp.dest('build/tags_replaced'));
+
+    gulp.src('after.html')
+    .pipe(htmlreplace({
+        'css':'css/all.css',
+        'js': 'js/minjs/after.js'
+    }))
+    .pipe(gulp.dest('build/tags_replaced'));
+
+
+    return gulp.src('before.html')
       .pipe(htmlreplace({
           'css':'css/all.css',
-          'js': 'js/minjs/all.js'
+          'js': 'js/minjs/before.js'
       }))
-      .pipe(gulp.dest('build/'));
+      .pipe(gulp.dest('build/tags_replaced'));
 });
 
 
@@ -68,9 +108,6 @@ gulp.task('copy', function () {
     gulp.src('images/**/*')
         .pipe(copy())
         .pipe(gulp.dest('build/images/'));
-    gulp.src('hidden_resources/**/*')
-        .pipe(copy())
-        .pipe(gulp.dest('build/hidden_resources/'));
     return gulp.src('scss/**/*')
         .pipe(copy())
         .pipe(gulp.dest('build/scss/'));
@@ -80,13 +117,54 @@ gulp.task('cleanup',function(){
     return del('build/');
 });
 
+gulp.task('minifyhtml', function() {
+    gulp.src('build/tags_replaced/during.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/hidden_resources'));
+
+    gulp.src('build/tags_replaced/after.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/hidden_resources'));
+
+    return gulp.src('build/tags_replaced/before.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build/hidden_resources'));
+});
+
+gulp.task('prepare_before', function () {
+    gulp.src('build/hidden_resources/before.html')
+    .pipe(copy())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('prepare_after', function () {
+    gulp.src('build/hidden_resources/after.html')
+    .pipe(copy())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('prepare_during', function () {
+    gulp.src('build/hidden_resources/during.html')
+    .pipe(copy())
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('cleanup_tags_replaced',function(){
+    return del('build/tags_replaced');
+});
+
 gulp.task('build', function(callback) {
     runSequence('cleanup',
                 'copy', 
-                'concatjs',
+                'concatjs_before',
+                'concatjs_during',
+                'concatjs_after',
                 'minifyjs',
                 'replacehtml',
                 'concatcss',
+                'minifycss',
+                'minifyhtml',
+                'cleanup_tags_replaced',
                 callback);
 });
 
---
+++ Markup/contribution/js/during-balance.js
@@ -0,0 +1,17 @@
+$(document).ready( function () {
+    var targetEth = 1000000;
+    $("#targetEth").html("<p class='barTargetAmount'>"+targetEth+"</p>");
+    $.ajax({url: "https://api.etherscan.io/api?module=account&action=balance&address=0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae&tag=latest&apikey=YourApiKeyToken", success: function(result){
+            var totalWei = result.result;
+            var totalEth = totalWei/1000000000000000000;
+            var totalEth = Math.round(totalEth);
+            $("#totalEth").html("<p class='barTotal'>"+totalEth+"</p>");
+            var progress = (totalEth/targetEth) * 100;
+            var pc = progress+"%";
+            $("#barProgressStatus").css({width:pc});
+        },
+        error: function(err){
+            console.log(err);
+        }
+    });
+ });
