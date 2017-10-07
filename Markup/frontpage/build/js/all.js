6747be32502e73a715622176048f88cc0a178f6d
kuba <getkuba@gmail.com>
2017-10-07 09:40:04 +0700

    build

--- Markup/frontpage/build/js/all.js
+++ Markup/frontpage/build/js/all.js
@@ -347,6 +347,16 @@ var header;
 $(document).ready(function () {
     header = new Header();
 });
+$(window).scroll(function (e) {
+    buttonParallax();
+});
+
+function buttonParallax() {
+    var scrolled = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    $('.moreSection').css('bottom', (scrolled * 0.9) + 30 + 'px');
+    $('.moreSection').css('opacity', 1 - ((scrolled * 1.9) / windowHeight));
+}
 $(window).on('load', function () {
     childEqualHeight('.teaserItem', '.teaserItemImage');
 });
